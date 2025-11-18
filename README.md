38517ce482c61e1b285a50e4c9e2f797538e56ad
albertbouvier646-creator <albertbouvier646@gmail.com>
2025-11-18 18:16:48 +0100

    Summary

---
+++ README.md
@@ -0,0 +1,1 @@
+Rebuilt project scaffolding created from your uploaded Base44 site. Review /components to refine layout and replace placeholder OriginalPage.
---
+++ app/page.jsx
@@ -0,0 +1,2 @@
+import OriginalPage from '../components/OriginalPage'
+export default function Page(){ return (<main className="min-h-screen"><OriginalPage /></main>) }
